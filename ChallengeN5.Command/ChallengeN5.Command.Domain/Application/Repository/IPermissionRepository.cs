52ee0ec820263df62c80ec05c44d9ac6f199068a
José Moya <moya_jose_575@hotmail.com>
2024-09-05 16:59:49 -0400

    chore: some changes

--- ChallengeN5.Command/ChallengeN5.Command.Domain/Application/Repository/IPermissionRepository.cs
+++ ChallengeN5.Command/ChallengeN5.Command.Domain/Application/Repository/IPermissionRepository.cs
@@ -1,8 +1,21 @@
 ﻿namespace ChallengeN5.Command.Domain.Application.Repository;
 
 using ChallengeN5.Command.Domain.Application.Model;
-using ChallengeN5.Command.Domain.Architecture.Repository;
 
-public interface IPermissionRepository : ICreateRepository<Permission, int>
+public interface IPermissionRepository
 {
+    public Task CreateAsync(Permission entity, CancellationToken cancellationToken);
+
+    public Task UpdateAsync(Permission entity, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Get permission by employee id and permission id
+    /// </summary>
+    /// <param name="employeeId">employee id</param>
+    /// <param name="permissionId">permission id
+    /// 
+    /// </param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public Task<Permission> GetAsync(int employeeId, int permissionId, CancellationToken cancellationToken);
 }
--- ChallengeN5.Command/ChallengeN5.Command.Persistance/Application/Repository/PermissionRepository.cs
+++ ChallengeN5.Command/ChallengeN5.Command.Persistance/Application/Repository/PermissionRepository.cs
@@ -4,10 +4,33 @@ using ChallengeN5.Command.Domain.Application.Model;
 using ChallengeN5.Command.Domain.Application.Repository;
 using ChallengeN5.Command.Persistance.Application.Data;
 using ChallengeN5.Command.Persistance.Architecture;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 public class PermissionRepository : CreateRepository<Permission, int>, IPermissionRepository
 {
     public PermissionRepository(N5Context context) : base(context)
     {
+    }
+
+    public IEnumerable<Permission> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Permission>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Permission? GetById(int key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Permission?> GetByIdAsync(int key, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
     }
 }
