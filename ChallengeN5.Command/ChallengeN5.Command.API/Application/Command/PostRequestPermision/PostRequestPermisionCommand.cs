79b732dc972059f8ad747dd1c2d29fb7317908eb
José Moya <moya_jose_575@hotmail.com>
2024-09-06 12:11:20 -0400

    chore: request permission

--- ChallengeN5.Command/ChallengeN5.Command.API/Application/Command/PostRequestPermision/PostRequestPermisionCommand.cs
+++ ChallengeN5.Command/ChallengeN5.Command.API/Application/Command/PostRequestPermision/PostRequestPermisionCommand.cs
@@ -2,6 +2,7 @@
 
 using ChallengeN5.Command.API.Architecture.Model;
 using MediatR;
+using System.ComponentModel.DataAnnotations;
 
 /// <summary>
 /// Post Request Permission Command
@@ -11,20 +12,24 @@ public class PostRequestPermisionCommand : BaseRequest, IRequest<BaseResponse>
     /// <summary>
     /// Employee Id
     /// </summary>
+    [Required]
     public int EmployeeId { get; set; }
 
     /// <summary>
     /// Permission Type Id
     /// </summary>
+    [Required]
     public int PermissionTypeId { get; set; }
 
     /// <summary>
     /// Date when the permission starts
     /// </summary>
+    [Required]
     public DateTime StartDate { get; set; }
 
     /// <summary>
     /// Date when the permission ends
     /// </summary>
+    [Required]
     public DateTime EndDate { get; set; }
 }
