99d271af81f3f7646e4b63c594977877201512f3
José Moya <moya_jose_575@hotmail.com>
2024-09-06 12:11:53 -0400

    feat: put permission ready

---
+++ ChallengeN5.Command/ChallengeN5.Command.API/Application/Command/PutPermission/PutPermissionHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace ChallengeN5.Command.API.Application.Command.PutPermission;
+
+using ChallengeN5.Command.API.Architecture.Model;
+using ChallengeN5.Command.Domain.Application.Model;
+using ChallengeN5.Command.Domain.Application.Repository;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Put permission handler
+/// </summary>
+public class PutPermissionHandler : IRequestHandler<PutPermissionCommand, BaseResponse>
+{
+    private readonly IPermissionRepository _permissionRepository;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    public PutPermissionHandler(IPermissionRepository permissionRepository)
+    {
+        _permissionRepository = permissionRepository;
+    }
+
+    /// <summary>
+    /// Handle the request
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <exception cref="KeyNotFoundException"></exception>
+    public async Task<BaseResponse> Handle(PutPermissionCommand request, CancellationToken cancellationToken)
+    {
+        Permission permission = await _permissionRepository.GetByIdAsync(request.PermissionId, cancellationToken) ??
+            throw new KeyNotFoundException($"The permission with id:{request.PermissionId}, don't exist.");
+        
+        UpdatePermissionDetails(request, permission);
+
+        await _permissionRepository.UpdateAsync(permission, cancellationToken);
+
+        return new BaseResponse
+        {
+            HttpCode = 200,
+            HttpMessage = "Permission was updated"
+        };
+    }
+
+    /// <summary>
+    /// update entity details
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="permission"></param>
+    private static void UpdatePermissionDetails(PutPermissionCommand request, Permission permission)
+    {
+        permission.StartDate = request.StartDate;
+        permission.EndDate = request.EndDate;
+        permission.IsActive = request.IsActive;
+    }
+}
--- ChallengeN5.Command/ChallengeN5.Command.API/Application/Controllers/PermissionController.cs
+++ ChallengeN5.Command/ChallengeN5.Command.API/Application/Controllers/PermissionController.cs
@@ -1,16 +1,17 @@
 ﻿namespace ChallengeN5.Command.API.Application.Controllers;
 
 using ChallengeN5.Command.API.Application.Command.PostRequestPermision;
+using ChallengeN5.Command.API.Application.Command.PutPermission;
 using ChallengeN5.Command.API.Architecture;
 using ChallengeN5.Command.API.Architecture.Model;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
 /// <summary>
 /// Permission Controller
 /// </summary>
+[Route("[controller]")]
 public class PermissionController : N5Controller
 {
     /// <summary>
@@ -22,7 +23,7 @@ public class PermissionController : N5Controller
     }
 
     /// <summary>
-    /// Obtener permisos del usuario logueado
+    /// Request a permission
     /// </summary>
     /// <remarks>
     /// Sample request:
@@ -43,6 +44,32 @@ public class PermissionController : N5Controller
     [ProducesResponseType(typeof(ErrorBaseResponse), (int)HttpStatusCode.NotFound)]
     [ProducesResponseType(typeof(ErrorBaseResponse), (int)HttpStatusCode.InternalServerError)]
     public async Task<ActionResult> RequestPermission([FromBody] PostRequestPermisionCommand command, CancellationToken cancellationToken)
+    {
+        return await RunHandler(command, cancellationToken);
+    }
+
+    /// <summary>
+    /// Update a permission
+    /// </summary>
+    /// <remarks>
+    /// Sample request:
+    ///
+    ///     PUT /permission
+    ///
+    /// </remarks>
+    /// <param name="command">body</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <response code="200">Success request</response>
+    /// <response code="400">The request can't be resolve.</response>
+    /// <response code="404">The request not found.</response>
+    /// <response code="500">Internal server error</response>
+    /// <returns>Create the resource</returns>
+    [HttpPut]
+    [ProducesResponseType(typeof(BaseResponse), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ErrorBaseResponse), (int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType(typeof(ErrorBaseResponse), (int)HttpStatusCode.NotFound)]
+    [ProducesResponseType(typeof(ErrorBaseResponse), (int)HttpStatusCode.InternalServerError)]
+    public async Task<ActionResult> UpdatePermission([FromBody] PutPermissionCommand command, CancellationToken cancellationToken)
     {
         return await RunHandler(command, cancellationToken);
     }
